b74afdb5a643ce5693d0edd3e97c07a79e7da691
yyyytawa <116234899+yyyytawa@users.noreply.github.com>
2025-07-21 22:37:27 +0800

    feat: 云湖用户信息检测(未完成)

---
+++ yh_user_info_monitor/config.py
@@ -0,0 +1,11 @@
+bot_token = ""    # BOT TOKEN
+monitored_list = {
+{
+  "monitored_id": 7354488,
+  "push": {
+    "group": [114514],
+    "user": [19180]
+  }
+}
+monitor_data_file = "monitor_data.json"
+monktored_list_url = "" # 填写被监控名单URL,不需要请留空,勿删除
