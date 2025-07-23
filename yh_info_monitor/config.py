cd08c8290ef4fb30d5dcacc6dc65c3b5cc98fe9f
yyyytawa <116234899+yyyytawa@users.noreply.github.com>
2025-07-23 16:09:04 +0800

    feat: 支持监控群聊和机器人信息,支持设置等待间隔

--- yh_info_monitor/config.py
+++ yh_info_monitor/config.py
@@ -1,9 +1,25 @@
-bot_token = "isjsnsnnsoqlnenfn009f3d3a"    # BOT TOKEN
-monitored_list = {
-  "7354488": {
+bot_token = "gtdcngdhjfdbd"    # BOT TOKEN
+# 监控列表与监控数据相关
+monitored_list = { # 不需要可留空
+  "6301414": {
+    "type": "user",
+    "user": [7354488],
+  },
+  "875050655": {
+    "type": "group",
+    "group": [257731539]
+  },
+  "34433153": {
+    "type": "bot",
     "user": [7354488],
     "group": [257731539]
   }
 }
 monitor_data_file = "monitor_data.json"
-monitored_list_url = "" # 填写被监控名单URL,不需要请留空,勿删除
+monitored_list_url = "" # 填写被监控名单URL
+# 等待时长相关
+time_per_object = 10 # 每两个检测对象等待时长
+time_per_check = 10 # 每两轮检测等待时长
+time_per_push = 1 # 每两次推送的等待时间
+# 日志相关
+log_file = "" # 日志文件名称,留空不保存日志文件,开启此项会导致控制台无输出,不推荐开启因为我没做日志大小限制和分割(
--- yh_info_monitor/main.py
+++ yh_info_monitor/main.py
@@ -7,8 +7,12 @@ import threading
 from datetime import datetime, timedelta
 import os
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+ format='%(asctime)s - %(levelname)s - %(message)s', filename = config.log_file, filemode="w")
 
+time_per_object = config.time_per_object
+time_per_check = config.time_per_check
+time_per_push = config.time_per_push
 monitor_data_file = config.monitor_data_file
 token = config.bot_token
 lock_data = threading.Lock()
@@ -84,7 +88,7 @@ def get_user_info(user_id):
         if data.get("code") == 1 and data.get("data") and data.get("data").get("user"):
             user_info = data["data"]["user"]
             return {
-              "nickname": user_info.get("nickname", ""),
+              "name": user_info.get("nickname", ""), # byd机器人和用户nickname,群聊name.
               "avatarUrl": user_info.get("avatarUrl", "")
             }
             
@@ -98,6 +102,57 @@ def get_user_info(user_id):
     except Exception as e:
         logging.error(f"发生错误: {e}")
         return None
+
+def get_group_info(group_id):
+    api = "https://chat-web-go.jwzhd.com/v1/group/group-info"
+    try:
+        post_data = json.dumps({"groupId": str(group_id)})
+        response = requests.post(api,data=post_data)
+        response.raise_for_status()
+        data = response.json()
+        if data.get("code") == 1 and data.get("data"):
+            group_info = data["data"]["group"]
+            return {
+              "name": group_info.get("name",""),
+              "avatarUrl": group_info.get("avatarUrl",""),
+              "introduction": group_info.get("introduction","")
+            }
+        elif data.get("code") != 1:
+            logging.error(f"服务端返回错误码: {data.get("code")},msg: {data.get("msg")}")
+            return None
+        else:
+            logging.error("API请求错误")
+            return None
+
+    except Exception as e:
+        logging.error(f"获取群聊信息失败: {e}")
+        return None
+
+def get_bot_info(bot_id):
+    api = "https://chat-web-go.jwzhd.com/v1/bot/bot-info"
+    try:
+        post_data = json.dumps({"botId": str(bot_id) })
+        response = requests.post(api, post_data)
+        logging.debug(response)
+        response.raise_for_status()
+        data = response.json()
+        if data.get("code") == 1 and data.get("data"):
+            bot_info = data["data"]["bot"]
+            return {
+              "name": bot_info.get("nickname",""),
+              "avatarUrl": bot_info.get("avatarUrl",""),
+              "introduction": bot_info.get("introduction","")
+            }
+              
+        elif data.get("code") != 1:
+            logging.error(f"服务端返回错误码: {data.get("code")} msg: {data.get("msg")}")
+            return
+        else:
+            logging.error("API错误")
+            return
+    except Exception as e:
+        logging.error(f"获取机器人信息失败: {e}")
+        return
 
 def push_msg(id,type,content,contenttype): # 发送消息的函数
     global token
@@ -123,26 +178,37 @@ def push_msg(id,type,content,contenttype): # 发送消息的函数
     except Exception as e:
         logging.error(f"错误: {e}")
 
-def make_html(id,old_name,new_name,old_avatar,new_avatar):
+def make_html(id,type,old_name,new_name,old_avatar,new_avatar,old_introduction, new_introduction, *update_time):
     update_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    if new_name != old_name:
+    if type == "user":
+        type_text = "用户"
+    elif type == "group":
+        type_text = "群聊"
+    elif type == "bot":
+        type_text = "机器人"
+    else:
+        logging.error(f"未知类型: {type}")
+        type_text = ""
+
+    if new_name != old_name: # 名称
         name_part = f"""
     <div style="margin-bottom: 12px;">
-        <span style="font-weight: bold; color: #586069;">名称变化:</span>
+        <span style="font-weight: bold; color: #586069;">{type_text}名称变化:</span>
         <span style="color: #24292e;">{old_name} → {new_name}</span>
     </div>
         """
     else:
         name_part = f"""  
     <div style="margin-bottom: 12px;">
-        <span style="font-weight: bold; color: #586069;">当前名称:</span>
+        <span style="font-weight: bold; color: #586069;">{type_text}当前名称:</span>
         <span style="color: #24292e;">{old_name}</span>
     </div>
          """
-    if new_avatar != old_avatar:
+
+    if new_avatar != old_avatar: # 头像
         avatar_part = f"""
         <div style="margin-bottom: 16px;">
-        <div style="font-weight: bold; color: #586069; margin-bottom: 4px;">头像更新:</div>
+        <div style="font-weight: bold; color: #586069; margin-bottom: 4px;">{type_text}头像更新:</div>
         <div style="display: flex; gap: 16px; align-items: center;">
             <div>
                 <div style="font-size: 13px; color: #586069; margin-bottom: 4px;">原头像</div>
@@ -159,19 +225,50 @@ def make_html(id,old_name,new_name,old_avatar,new_avatar):
     else:
         avatar_part = f"""
     <div style="margin-bottom: 16px;">
-        <div style="font-weight: bold; color: #586069; margin-bottom: 4px;">目前头像:</div>
+        <div style="font-weight: bold; color: #586069; margin-bottom: 4px;">{type_text}目前头像:</div>
         <img src="{old_avatar}" style="width: 50px; height: 50px; border-radius: 4px; border: 1px solid #e1e4e8;">
     </div>
         """
+
+    if (type == "group" or type == "bot") and new_introduction != old_introduction: # 简介
+        introduction_part = f"""
+    <div style="margin-bottom: 16px;">
+        <div style="font-weight: bold; color: #586069; margin-bottom: 8px;">{type_text}简介变化:</div>
+        <div style="background-color: #ffffff; border: 1px solid #e1e4e8; border-radius: 4px; padding: 12px; margin-bottom: 8px;">
+            <div style="font-size: 13px; color: #586069; margin-bottom: 4px;">原简介:</div>
+            <div style="color: #24292e; line-height: 1.4;">{old_introduction}</div>
+        </div>
+        <div style="text-align: center; margin: 8px 0;">
+            <div style="font-size: 16px; color: #586069;">↓</div>
+        </div>
+        <div style="background-color: #ffffff; border: 1px solid #e1e4e8; border-radius: 4px; padding: 12px;">
+            <div style="font-size: 13px; color: #586069; margin-bottom: 4px;">新简介:</div>
+            <div style="color: #24292e; line-height: 1.4;">{new_introduction}</div>
+        </div>
+    </div>
+        """
+    elif (type == "group" or type == "user") and old_introduction != "" and new_introduction == old_introduction:
+        introduction_part =f"""
+    <div style="margin-bottom: 16px;">
+        <div style="font-weight: bold; color: #586069; margin-bottom: 8px;">当前简介:</div>
+        <div style="background-color: #ffffff; border: 1px solid #e1e4e8; border-radius: 4px; padding: 12px;">
+            <div style="color: #24292e; line-height: 1.4;">{old_introduction}</div>
+        </div>
+    </div>
+    """
+    else:
+        introduction_part = ""
+
     html_content = f"""<div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; border: 1px solid #e1e4e8; border-radius: 6px; padding: 16px; background-color: #f6f8fa;">
-    <h2 style="color: #24292e; margin-top: 0; border-bottom: 1px solid #e1e4e8; padding-bottom: 8px;">监控信息更新</h2>
+    <h2 style="color: #24292e; margin-top: 0; border-bottom: 1px solid #e1e4e8; padding-bottom: 8px;">{type_text}信息更新</h2>
     
     <div style="margin-bottom: 12px;">
-        <span style="font-weight: bold; color: #586069;">ID:</span>
+        <span style="font-weight: bold; color: #586069;">{type_text}ID:</span>
         <span style="color: #24292e;">{id}</span>
     </div>
     {name_part}
     {avatar_part}
+    {introduction_part}
     <div style="font-size: 12px; color: #586069; text-align: right; border-top: 1px solid #e1e4e8; padding-top: 8px;">
         更新时间: {update_time}
     </div>
@@ -179,63 +276,83 @@ def make_html(id,old_name,new_name,old_avatar,new_avatar):
     return html_content
 
 def monitor_thread_instance():
-    global monitor_data, monitored_list
+    global monitor_data, monitored_list, time_per_check, time_per_object, time_per_push
     logging.info("监控线程开始启动")
     while True:
         try:
             list_to_check = list(monitored_list.keys())
             logging.info(f"开启本轮检查,共 {len(list_to_check)} 个对象")
-            for user_id in monitored_list:
-                old_user_info = monitor_data.get(user_id, {})
-                logging.info(f"正在获取 {user_id} 信息")
-                current_info = get_user_info(user_id)
+            for id in monitored_list:
+                old_info = monitor_data.get(id, {})
+
+                type = monitored_list.get(id).get("type","user")
+                if type == "user":
+                    current_info = get_user_info(id)
+                    type_text = "用户"
+                elif type == "group":
+                    current_info = get_group_info(id)
+                    type_text = "群聊"
+                elif type == "bot":
+                    current_info = get_bot_info(id)
+                    type_text = "机器人"
+                else:
+                    logging.error(f"未知类型: {type}")
+                    type_text = "未知"
+
+                logging.info(f"正在获取{type_text}: {id} 信息")
                 if not current_info:
-                    logging.error(f"无法获取用户 {user_id} 信息,跳过")
+                    logging.error(f"无法获取{type_text}: {id} 信息,跳过")
                     continue
                 
-                if not old_user_info:
-                    logging.info(f"首次记录用户 {user_id} 的信息。")
+                if not old_info:
+                    logging.info(f"首次记录{type_text}: {id} 的信息")
                     with lock_data:
-                        monitor_data[user_id] = {
-                            "nickname": current_info["nickname"],
+                        monitor_data[id] = {
+                            "name": current_info["name"],
                             "avatarUrl": current_info["avatarUrl"],
+                            "introduction": current_info.get("introduction","")
                         }
                     save_monitor_data()
-                    time.sleep(10)
+                    time.sleep(time_per_object)
                     continue
 
-                nickname_changed = current_info.get("nickname") != old_user_info.get("nickname")
-                avatar_changed = current_info.get("avatarUrl") != old_user_info.get("avatarUrl")
-                changed = nickname_changed or avatar_changed
+                name_changed = current_info.get("name") != old_info.get("name")
+                avatar_changed = current_info.get("avatarUrl") != old_info.get("avatarUrl")
+                introduction_changed = current_info.get("introduction","") != old_info.get("introduction","")
+                changed = name_changed or avatar_changed or introduction_changed
                 
                 if changed:
                     msg_content = make_html(
-                        user_id,
-                        old_user_info.get("nickname"),
-                        current_info.get("nickname"),
-                        old_user_info.get("avatarUrl"),
-                        current_info.get("avatarUrl")
+                        id,
+                        type = type,
+                        old_name = old_info.get("name"),
+                        new_name = current_info.get("name"),
+                        old_avatar = old_info.get("avatarUrl"),
+                        new_avatar = current_info.get("avatarUrl"),
+                        old_introduction = old_info.get("introduction"),
+                        new_introduction = current_info.get("introduction")
                     )
 
-                    for notify_group in monitored_list.get(user_id).get("group",""):
+                    for notify_group in monitored_list.get(id,[]).get("group",""):
                         push_msg(notify_group,"group",msg_content,"html")
-                        logging.info(f"推送 {user_id} 信息到群组 {notify_group}")
-                        time.sleep(1)
-                    for notify_user in monitored_list.get(user_id).get("user",""):
+                        logging.info(f"推送 {id} 信息到群组 {notify_group}")
+                        time.sleep(time_per_push)
+                    for notify_user in monitored_list.get(id).get("user",[]):
                         push_msg(notify_user,"user",msg_content,"html")
-                        logging.info(f"推送 {user_id} 信息到用户 {notify_user}")
-                        time.sleep(1)
+                        logging.info(f"推送 {id} 信息到用户 {notify_user}")
+                        time.sleep(time_per_push)
                         
                     with lock_data:
-                        monitor_data[user_id]["nickname"] = current_info["nickname"]
-                        monitor_data[user_id]["avatarUrl"] = current_info["avatarUrl"]
+                        monitor_data[id]["name"] = current_info["name"]
+                        monitor_data[id]["avatarUrl"] = current_info["avatarUrl"]
+                        monitor_data[id]["introduction"] = current_info.get("introduction","")
                     save_monitor_data()
                 else:
-                    logging.info(f"{user_id} 信息无变化")
-                time.sleep(10) # 每个用户间隔
+                    logging.info(f"{type_text}: {id} 信息无变化")
+                time.sleep(time_per_object)
 
-            logging.info("本轮用户检查完成")
-            time.sleep(10) # 每次检查间隔
+            logging.info("本轮检查完成")
+            time.sleep(time_per_check) # 每次检查间隔
                 
         except Exception as e:
             logging.error(f"监控线程出错: {str(e)}")
@@ -262,6 +379,6 @@ if __name__ == "__main__":
             sync_monitor_data()
             time.sleep(3600)
     except KeyboardInterrupt:
-        logging.info("程序被终止。")
+        logging.info("程序被终止")
     except Exception as e:
         logging.error(f"主线程异常: {e}")
